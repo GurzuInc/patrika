a6d31e003eb34c52af0f77bf4b8c9cd63ba63f3f
gkunwar <gkunwar09@gmail.com>
2015-09-20 14:13:33 +0545

    Removed unnecessary line of code

--- lib/announcement.rb
+++ lib/announcement.rb
@@ -5,10 +5,10 @@ CONFIG=Dotenv.load
 class Announcement
   attr_accessor :key, :secret
   def initialize
-    config = YAML.load_file('config/configurations.yml')
     @key = CONFIG['trello_key']
     @secret = CONFIG['trello_secret']
   end
+
 end
 obj = Announcement.new
 
