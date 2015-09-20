68229c2ec8761c0e75e09d5a201bc31110e1ef36
gkunwar <gkunwar09@gmail.com>
2015-09-20 16:16:56 +0545

    Added API token to Trello

--- lib/announcement.rb
+++ lib/announcement.rb
@@ -5,15 +5,16 @@ require 'httparty'
 CONFIG=Dotenv.load
 
 class Announcement
-  attr_accessor :key, :secret
+  attr_accessor :key, :secret, :token
 
   def initialize
     @key = CONFIG['trello_key']
     @secret = CONFIG['trello_secret']
+    @token = CONFIG['trello_token']
   end
 
   def fetch_card
-    list = HTTParty.get("https://api.trello.com/1/lists/55fe6d9b3eca2cb147724882?fields=name&cards=open&key=#{@key}")
+    list = HTTParty.get("https://api.trello.com/1/lists/55fe6d9b3eca2cb147724882?fields=name&cards=open&key=#{@key}&token=#{@token}")
     cards = list['cards']
     result = Array.new
     cards.each do |card|
@@ -32,6 +33,6 @@ class Announcement
   # Method to move published card from ready to publish to published list. This is not working now.
   def move_card(card)
     id = card['id']
-    HTTParty.put("https://api.trello.com/1/cards/#{id}/55fe79ff6286c04f8ee97091&key=#{@key}")
+    HTTParty.put("https://api.trello.com/1/cards/#{id}/55fe79ff6286c04f8ee97091&key=#{@key}&token=#{@token}")
   end
 end
