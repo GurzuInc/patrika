c0a55bf4cdae2991930f50a30864c528b7d928d2
kapil <kapilnakhwa@gmail.com>
2015-09-20 13:53:26 +0545

    httparty added

--- lib/knowledge_ninja.rb
+++ lib/knowledge_ninja.rb
@@ -1,9 +1,14 @@
+require File.join(File.dirname(__FILE__),'configurations.rb')
 class KnowledgeNinja
   def initialize
     @token = CONFIG['slack_authorization_token']
   end
 
-  def tannum
+  def teaminfo
+  end
+
+  def channel
+    
   end
 
 end 
