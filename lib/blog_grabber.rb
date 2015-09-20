185731f1a15cfb00854fdffad39b7a620bcd9c3b
sagun shrestha <sagun@jyaasa.com>
2015-09-20 14:48:03 +0545

    Scrapped this months posts contents
    Added a method which scraps this months post's links and contents and push an array [post_link, post_content] to @this_months_link array.

--- lib/blog_grabber.rb
+++ lib/blog_grabber.rb
@@ -12,11 +12,17 @@ class BlogGrabber
   def get_this_months_post
     @node.each do |link|
       link_contents = Mechanize::Page::Link.new(link, @mechanize, @page)
-      post_date_str = link_contents.click.search(".blog-layout .right-sidebar h3").first.text
-      link.attributes["href"].value = "http://jyaasa.com" + link.attributes["href"].value
-      @this_months_link << link if is_this_months_post?(post_date_str)
+      clicked_link = link_contents.click
+      post_date_str = clicked_link.search(".blog-layout .right-sidebar h3").first.text
+      @this_months_link.push(get_link_and_content(clicked_link, link)) if is_this_months_post?(post_date_str)
     end
     @this_months_link
+  end
+
+  def get_link_and_content(clicked_link, link)
+    content = clicked_link.search(".blog-layout .right-sidebar p").first.text[0..125] + ".."
+    link = link.attributes["href"].value = "http://jyaasa.com" + link.attributes["href"].value
+    [link, content]
   end
 
   def is_this_months_post?(date_str)
@@ -25,5 +31,5 @@ class BlogGrabber
   end
 end
 
-
 this_months_post_links = BlogGrabber.new.get_this_months_post
+puts this_months_post_links
