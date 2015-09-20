0633b505ceddf092d34778a2639af6950a374dd5
kapil <kapilnakhwa@gmail.com>
2015-09-20 16:30:22 +0545

    formatter compelted for knowledge

--- lib/formatters/knowledge_formatter.rb
+++ lib/formatters/knowledge_formatter.rb
@@ -1,52 +1,29 @@
+require 'mechanize'
 class KnowledgeFormatter
   def initialize(knowledge)
     @knowledge = knowledge
+    @link = nil
+    @title = nil
+    @text = nil
   end
 
   def format
-    [get_title, URI.extract(@knowledge['text']).sample, humanize(@knowledge['text']) ]
+    set_link
+    set_title_and_text
+    return [@title, @link, @text]    
   end
 
-  def get_title
-    if @knowledge['attachments']
-      title = @knowledge['attachments'].first['text']
-    else
-      title = "Come find me luke!"
-    end
-    return title
+  def set_title_and_text
+    agent = Mechanize.new
+    page = agent.get(@link)
+    @title = page.title
+    @text = page.search('body').text.gsub('\n',' ').slice(20..100)  # THIS IS VERY BASIC AND ROUGH 
   end
 
-  def humanize (value, options = {})
-    if options.empty?
-      options[:format] = :sentence
-    end
-    values = []
-    if value.include? '_'
-      values = value.split('_')
-      values.each { |v| v.downcase! }  
-      if options[:format] == :allcaps
-        values.each do |value|
-          value.capitalize!
-        end
-        if options.empty?
-          options[:seperator] = " "
-        end
-        return values.join " "
-      end
-      if options[:format] == :class
-        values.each do |value|
-          value.capitalize!
-        end
-        return values.join ""
-      end
-
-      if options[:format] == :sentence
-        values[0].capitalize!
-        return values.join " "
-      end
-      if options[:format] == :nocaps
-        return values.join " "
-      end
-    end
+  def set_link
+    @link = URI.extract(@knowledge['text']).sample
   end
+
+  
+  
 end
