a154e989187e3cb7468786e4fc92e396fdd626ea
gkunwar <gkunwar09@gmail.com>
2015-09-20 13:20:23 +0545

    Initialize accnouncement baseics attributes

--- lib/announcement.rb
+++ lib/announcement.rb
@@ -1,6 +1,12 @@
+require 'yaml'
+require 'pry'
 class Announcement
   attr_accessor :key, :secret
   def initialize
-
+    config = YAML.load_file('config/configurations.yml')
+    @key = config['trello']['key']
+    @secret = config['trello']['secret']
   end
 end
+obj = Announcement.new
+
