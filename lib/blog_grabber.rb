cfd454af749fe47b7d057c22ccbe7e8bc9c1f3cd
sagun shrestha <sagun@jyaasa.com>
2015-09-21 09:30:29 +0545

    changed condition to check date of the post. If post is previous months post then its included in newsletter

--- lib/blog_grabber.rb
+++ lib/blog_grabber.rb
@@ -14,8 +14,7 @@ class BlogGrabber
       link_contents = Mechanize::Page::Link.new(link, @mechanize, @page)
       clicked_link = link_contents.click
       post_date_str = clicked_link.search(".blog-layout .right-sidebar h3").first.text
-      @this_months_link.push(get_link_and_content(clicked_link, link)) 
-      # if is_this_months_post?(post_date_str)
+      @this_months_link.push(get_link_and_content(clicked_link, link)) if is_this_months_post?(post_date_str)
     end
     @this_months_link
   end
@@ -29,6 +28,6 @@ class BlogGrabber
 
   def is_this_months_post?(date_str)
     post_month = Date.parse(date_str).month
-    post_month == Date.today.month ? true : false
+    post_month == Date.today.month - 1 ? true : false
   end
 end
