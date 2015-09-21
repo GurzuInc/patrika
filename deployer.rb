177e7d12b1b2da304564092bb45a13a64da323e8
gkunwar <gkunwar09@gmail.com>
2015-09-21 10:23:02 +0545

    Set the server detail to .env file

--- deployer.rb
+++ deployer.rb
@@ -1,7 +1,9 @@
+require File.join(File.dirname(__FILE__),'lib/configurations.rb')
 class Deployer
   def deploy
     source = File.expand_path('./')
-    response = `rsync -avuz --progress #{source}/ deployer@kapilrajnakhwa.com:/home/deployer/tools/news-letter/`
+    server = CONFIG['server']
+    response = `rsync -avuz --progress #{source}/ #{server}:/home/deployer/tools/news-letter/`
     puts response
   end
 end
